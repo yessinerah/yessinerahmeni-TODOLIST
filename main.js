31ec4168c843fce052f29fe15c45bc7755c94a20
yessinerah <ahmed.rahmeni2016@gmail.com>
2023-11-27 16:58:06 +0100

    still not complete

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+ $('#button1').click(function () {
+    var add=$('input[name=listof]').val();
+    if($('input[name=listof]').val()===''){
+        alert('please write something !')
+     }
+     else
+ {$('ol').append('<li >'+add+'</li><button id="butt" ></button>')
+
+}
+$('#butt').click(function(){
+  
+    add.css('text-decoration','line-through')   
+})
+ });  
+//when user write something and press the button,the text will be added to a list and displayed under the white background with a checkbox and when there is no input it will be an alert
+
+
+
+
 
